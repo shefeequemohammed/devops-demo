a01356a1dd56a58d274a8f5811ebc858bcba7ace
shefzee <shefzee07@gmail.com>
2019-10-19 18:07:16 +0400

    changed packaging

--- src/main/java/com/shefzee/devopsdemo/HomeController.java
+++ src/main/java/com/shefzee/devopsdemo/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public ResponseEntity hello(){
 
         return ResponseEntity.ok("Hello Shefeeque");
