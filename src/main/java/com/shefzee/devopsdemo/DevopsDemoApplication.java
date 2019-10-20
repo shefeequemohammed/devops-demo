9a38d2e22cd6566f87e5af424fed734a046a7cde
shefzee <shefzee07@gmail.com>
2019-10-20 21:28:39 +0400

    Servlet initializer

--- src/main/java/com/shefzee/devopsdemo/DevopsDemoApplication.java
+++ src/main/java/com/shefzee/devopsdemo/DevopsDemoApplication.java
@@ -2,9 +2,10 @@ package com.shefzee.devopsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DevopsDemoApplication {
+public class DevopsDemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsDemoApplication.class, args);
